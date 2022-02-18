c96a9bd7dff6733d9d093e07c61230f0055c88cd
IT-SNK <90689814+IT-SNK@users.noreply.github.com>
2022-02-18 14:21:13 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,21 @@
+// let div = document.createElement('div');
+// div.className = "header";
+// div.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
+
+// document.body.append(div);
+
+// let nameAge = document.querySelector('.header')
+// nameAge.innerHTML = "<h1>salamaleikum</h1>";
+
+// document.body.appendChild(nameAge);
+
+
+// let quashtn = prompt('сколько тебе лет?');
+// let age = 20;
+
+// if(quashtn >= age){
+// alert('тебе можно в кино')
+// }else if(quashtn <= age){
+//     alert('нельзя в кино ');
+// };
+
